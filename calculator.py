f530fdcfb64646a2721753f588af47e3e160f21f
ryanbos@ufl.edu <ryanbos@ufl.edu>
2026-04-09 10:42:52 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import math
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -5,7 +6,38 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
+def add(a, b):
+    answer = a + b
+    return answer
     pass
 
+def subtract(a, b):
+    answer = a - b
+    return answer
+    pass
 
+def multiply(a, b):
+    answer = a * b
+    return answer
+    pass
+
+def divide(a, b):
+    if a == 0:
+        print("ZeroDivisionError")
+    else:
+        answer = b / a
+    return answer
+    pass
+
+def log(a, b):
+    if a < 0:
+        print("LogarithmicError")
+    else:
+        answer = math.log(a, b)
+        return answer
+    pass
+
+def exponent(a, b):
+    answer = a ** b
+    return answer
+    pass
