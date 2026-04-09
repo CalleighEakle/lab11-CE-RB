4e88fdf71ffe9df3f30384175d55a4bc9a969d8f
Calleigh Eakle <calleigheakle@ufl.edu>
2026-04-09 10:48:05 -0400

    modified calculator p2

--- calculator.py
+++ calculator.py
@@ -1,3 +1,7 @@
+# https://github.com/CalleighEakle/lab11-CE-RB
+# Partner 1: Ryan Bos
+# Partner 2: Calleigh Eakle
+
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -6,6 +10,21 @@ One function per operation, in order.
 """
 # First example
 def add(a, b): 
-    pass
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    raise ZeroDivisionError if a == 0 else b / a
+
+def log(a, b):
+    raise ValueError if a <= 0 else math.log(b, a)
+
+def exp(a, b):
+    return a ** b
 
 
